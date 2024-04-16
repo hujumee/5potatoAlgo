aa09a77b2fc8463181cdfee394f9e7f8e31704de
Youjung Heo <81809575+hujumee@users.noreply.github.com>
2024-04-16 10:00:54 +0200

    [Silver II] Title: 나무 자르기, Time: 5000 ms, Memory: 143264 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,25 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+trees = list(map(int, sys.stdin.readline().split()))
+
+start, end = 0, max(trees)
+res = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    cnt = 0
+    for elem in trees:
+        if elem > mid:
+            cnt += elem - mid
+    if cnt == M:
+        res = mid
+        break
+    elif cnt > M:
+        start = mid + 1
+    else:
+        end = mid - 1
+    res = end
+
+print(res)    
+    
